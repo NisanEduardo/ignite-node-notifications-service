f469b0e94ee55e57cad9e3c1e1868f6b58b6df69
Edu Leite <nisaneduardo@gmail.com>
2022-12-27 23:56:43 -0300

    criando os primeiros testes

--- src/app/entities/notification.ts
+++ src/app/entities/notification.ts
@@ -1,28 +1,53 @@
+import { Content } from './content';
+
 export interface NotificationProps {
-  content: string;
+  recipientId: string;
+  content: Content;
   category: string;
+  readAt?: Date | null;
+  createdAt: Date;
 }
 
 export class Notification {
   private props: NotificationProps;
 
-  constructor() {
-    this.content = '';
+  constructor(props: NotificationProps) {
+    this.props = props;
   }
 
-  public set content(content: string) {
-    if (content.length < 5) {
-      throw new Error();
-    }
+  public set recipientId(recipientId: string) {
+    this.props.recipientId = recipientId;
+  }
 
+  public get recipientId(): string {
+    return this.props.recipientId;
+  }
+
+  public set content(content: Content) {
     this.props.content = content;
   }
 
-  public get content(): string {
+  public get content(): Content {
     return this.props.content;
   }
+
+  public set category(category: string) {
+    this.props.category = category;
+  }
+
+  public get category(): string {
+    return this.props.category;
+  }
+
+  public set readAt(readAt: Date | null | undefined) {
+    this.props.readAt = readAt;
+  }
+
+  public get readAt(): Date | null | undefined {
+    return this.props.readAt;
+  }
+
+  public get createdAt(): Date {
+    return this.props.createdAt;
+  }
 }
-
-const notification = new Notification();
-
-notification.content = 'essa é minha notificação';

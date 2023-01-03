f350c2418fd87e1333bd97a2726932809dbc553f
Edu Leite <nisaneduardo@gmail.com>
2023-01-02 23:59:49 -0300

    criando testes e casos de uso

---
+++ src/app/entities/notification.spec.ts
@@ -0,0 +1,13 @@
+import { Content } from './content';
+import { Notification } from './notification';
+
+describe('notification', () => {
+  it('should be able to create a Notification', () => {
+    const notification = new Notification({
+      content: new Content('Nova solicitação de amizade'),
+      category: 'social',
+      recipientId: 'example-recipient-id',
+    });
+    expect(notification).toBeTruthy();
+  });
+});
